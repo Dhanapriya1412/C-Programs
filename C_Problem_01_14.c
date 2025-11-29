76b98844634bfb3a60d09c26d21b9ea7e38cc387
Dhanapriya Dhanaraj <dhanapriya1412@gmail.com>
2025-11-29 15:41:01 +0530

    Add files via upload

---
+++ C_Problem_01_14.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+    int x,y;
+
+    printf("Enter Number:");
+
+    scanf("%d",&x);
+
+    y= (x%10)*100 + ((x/10)%10)*10 + (x/100);
+
+    printf("Result = %d",y);
+
+    return 0;
+}
