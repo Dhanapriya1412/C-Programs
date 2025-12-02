5b2ea5aae4f0448d99e7eb0528b530e0ba0c450d
Dhanapriya Dhanaraj <dhanapriya1412@gmail.com>
2025-12-02 17:27:46 +0530

    Refactor loop logic and output in C program
    Updated loop logic to accumulate sum and improved output formatting.

--- C_Problem_04_10.c
+++ C_Problem_04_10.c
@@ -3,11 +3,15 @@ int main()
 {
     int x, sum;
     sum = 0;
-    x = 71;        
-loop: if(x < 80)
+    x = 71;
+
+loop:
+    if(x < 80)
     {
-        sum = sum + x;    
-        x = x + 2;        
+        sum = sum + x; 
+        x = x + 2;    
+        goto loop;    
     }
-    printf("%d", sum);
+    printf("%d\n", sum); 
+    return 0;
 }
