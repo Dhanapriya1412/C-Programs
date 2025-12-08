fdf9e8c884bd52ba6778b12bed72e962bb0a88d5
Dhanapriya Dhanaraj <dhanapriya1412@gmail.com>
2025-12-08 21:10:44 +0530

    Add files via upload

---
+++ C_Problem_02_11.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    int x, y;
+    printf("Enter number:");
+    scanf("%d", &x);       
+    y = (x /10 <= x % 10); 
+    printf("Result=%d\n", y);      
+    return 0;
+}
